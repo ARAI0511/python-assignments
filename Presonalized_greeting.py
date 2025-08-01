6ff0ca58fc540f94ee130565221e347ce6f6ec70
ARAI0511 <raiaditya0511@gmail.com>
2025-08-01 16:37:46 +0530

    Add files via upload
    Assignment 1- Basic Python Concepts.pdf
    task1 and task2

---
+++ Presonalized_greeting.py
@@ -0,0 +1,6 @@
+s1=input("Enter Your First Name:")
+s2=input("Enter Your Last Name:")
+
+str= s1 +" "+ s2
+
+print(f"Hello, {str}! Welcome to the Python Program")
---
+++ maths_operation.py
@@ -0,0 +1,14 @@
+a1=int(input("Enter The First Number :"))
+a2=int(input("Enter The Second Number :"))
+
+add=a1+a2
+print("Addition: ",add)
+
+sub=a1-a2
+print("Substraction: ",sub)
+
+div=a1//a2
+print("Division: ",div)
+
+mul=a1*a2
+print("Multiplication: ",mul)
